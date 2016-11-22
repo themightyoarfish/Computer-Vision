40c5209dd58fc5e8d09303b7cc1d24572d1591ff
themightyoarfish <rdiederichse@uos.de>
2016-11-22 22:27:59 +0100

    add py version

---
+++ Exercise3/sheet03.py
@@ -0,0 +1,171 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage.morphology as morph
+
+def my_boundary(img, ksize=(10,10)):
+    """
+    Compute boundary of binary image.
+
+    Parameters
+    ----------
+    img : ndarray of bools
+        A binary image.
+        
+    Returns
+    -------
+    boundary : ndarray of bools
+        The boundary as a binary image.
+    """
+    
+    return img ^ morph.binary_erosion(img, np.ones(ksize))
+    
+    
+# img = plt.imread("engelstrompete.png") > 0
+# plt.gray()
+# plt.imshow(my_boundary(img))
+# plt.show()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import morphology as morph, generate_binary_structure
+
+def my_distance_transform(img, **kwargs):
+    """
+    Distance transform of binary image.
+
+    Parameters
+    ----------
+    img : ndarray of bools
+        A binary image.
+        
+    Returns
+    -------
+    dt : ndarray of ints
+        The distance transform of the input image.
+    """
+
+    dt1 = np.zeros(img.shape, np.int32)
+    c = 1
+    img_wo_boundary = img.copy()
+    # while not completely eroded
+    while np.any(img_wo_boundary):
+        # find boundary
+        boundary = my_boundary(img_wo_boundary, **kwargs)
+        img_wo_boundary ^= boundary # shave of current boundary
+        dt1[boundary] = c # boundary can be used as mask
+        c += 1 # make brighter with every step
+
+    result = dt1
+    return result / result.max()
+
+def my_generalized_distance_transform(img, **kwargs):
+    dt1 = np.zeros(img.shape, np.int32)
+    c = 1
+    img_wo_boundary = img.copy()
+    # while not completely eroded
+    while np.any(img_wo_boundary):
+        # find boundary
+        boundary = my_boundary(img_wo_boundary, **kwargs)
+        img_wo_boundary ^= boundary # shave of current boundary
+        dt1[boundary] = c # boundary can be used as mask
+        c += 1 # make brighter with every step
+
+    dt2 = np.zeros(img.shape, np.int32)
+    c = 1
+    img_wo_boundary = ~img.copy()
+    # while not completely eroded
+    while np.any(img_wo_boundary):
+        # find boundary
+        boundary = my_boundary(img_wo_boundary, **kwargs)
+        img_wo_boundary ^= boundary # shave of current boundary
+        dt2[boundary] = c # boundary can be used as mask
+        c += 1 # make brighter with every step
+    
+    result = dt1 - dt2
+    # m = result.min()
+    # result = result + (- m if m < 0 else m)
+    return result / result.max()
+
+
+# img = plt.imread("engelstrompete.png") > 0
+# plt.gray()
+# plt.imshow(my_distance_transform(img, ksize=(10,10)))
+# plt.show()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage.morphology as morph
+
+
+def my_morph(A,B,ratio):
+    """
+    Morphing from binary image A to image B.
+
+    Parameters
+    ----------
+    A : ndarray of bools
+        A binary image (start).
+    B : ndarray of bools
+        A binary image (target), same shape as A.
+    ratio : float from 0.0 to 1.0
+        The ratio of image A and image B.
+        0.0=only image A, 1.0=only image B.
+        
+    Returns
+    -------
+    morph : ndarray of bools
+        A binary intermediate image between A and B.
+    """
+
+    d_a = my_generalized_distance_transform(A)
+    d_b = my_generalized_distance_transform(B)
+
+    result = (ratio * d_b + (1 - ratio) * d_a)
+    return result > 0
+
+img1 = plt.imread("kreis.png") > 0
+img2 = plt.imread("engelstrompete.png") > 0
+
+plt.gray()
+for i, ratio in enumerate(np.linspace(0, 1, 6), 1):
+    plt.subplot(2, 3, i)
+    plt.imshow(my_morph(img1, img2, ratio))
+    plt.axis('off')
+plt.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage.morphology as morph
+
+# def my_skeletonize(img):
+#     """
+#     Compute the skeloton of a binary image using hit_or_miss operator.
+    
+#     Parameters
+#     ----------
+#     img : ndarray of bools
+#         Binary image to be skeletonized.
+    
+#     Returns
+#     -------
+#     skeleton : ndarray of bools
+#         The skeleton of the input image.
+#     """
+    
+#     skeleton = np.zeros(img.shape,np.bool)
+    
+#     # FIXME: put your code here!
+
+
+#     return skeleton
+
+# img = plt.imread("engelstrompete.png") > 0
+# skel = my_skeletonize(img)
+# result = morph.distance_transform_cdt(img, metric = 'taxicab') + 50*img
+# result[morph.binary_dilation(skel)] = 0
+# plt.gray()
+# plt.imshow(result)
+# plt.show()
+
